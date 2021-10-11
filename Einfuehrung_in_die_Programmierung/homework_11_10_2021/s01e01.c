6093e0f6274c33d48272f7d54695d8af08d8d3d0
SourceCodeEngineer <santino.pircher.programmierer@gmail.com>
2021-10-11 22:34:22 +0200

    Added homework

---
+++ Einfuehrung_in_die_Programmierung/homework_11_10_2021/s01e01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Main Funktion die beim Ausführen des Programmes ausgeführt wird.
+
+int main(void) {
+    //Hier wird ein Text in der Kommandozeile ausgegeben.
+    printf("Hello World\n");
+    return EXIT_SUCCESS;
+}
---
+++ Einfuehrung_in_die_Programmierung/homework_11_10_2021/s01e04.c
@@ -0,0 +1,7 @@
+#include <stdio.h>              //inkludieren der standard bibliothek stdio.h was den input und output printf zulässt
+#include <stdlib.h>             //inkludieren der standard bibliothek stdlib.h was das keywort "EXIT_SUCCESS" zulässt
+
+int main(void) {                //main funktion definieren mit void (leere)
+    printf("Hello World!\n");   //Ausgabe der Hello World funktion
+    return EXIT_SUCCESS;        //rückgabe des wertes 0 was mit "EXIT_SUCCESS" über die Standardbibliothek stdlib.h passiert.
+}
