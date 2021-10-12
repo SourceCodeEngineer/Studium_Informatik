7355f6302e3588ca276c1c4b1c9cc75d231e6c1b
SourceCodeEngineer <santino.pircher.programmierer@gmail.com>
2021-10-12 14:09:07 +0200

    Added homework from today

--- Funktionale_Programmierung/Homework_12_10_2021/homework_12_10_2021.hs
+++ Funktionale_Programmierung/Homework_12_10_2021/homework_12_10_2021.hs
@@ -1,1 +1,6 @@
---test
+-- Aufgabe 1 
+-- 1.1 0 :: Bool --> 0 is not a Bool because it is a number!
+-- 1.2 head "test" : Char --> Head gives back the first character of a string and "t" is a Char!
+-- 1.3 'hello' :: String --> is not a string because of the wrong quotation marks!
+-- 1.4 plus1 :: Integer -> Integer --> Not valid, because plus1 does take the input float as well!
+
