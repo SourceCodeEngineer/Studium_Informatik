640d9789ec920876651a8c7ecba687e8540af76f
SourceCodeEngineer <santino.pircher.programmierer@gmail.com>
2021-11-01 10:12:37 +0100

    finished homework

--- Funktionale_Programmierung/Homework_26_10_2021/homework_26_10_2021.hs
+++ Funktionale_Programmierung/Homework_26_10_2021/homework_26_10_2021.hs
@@ -12,33 +12,39 @@ prefixes l@x = l : prefixes (init x)
 menu :: Char -> [a] -> Either String [[a]]
 menu 'p' l = Right (prefixes l)
 menu 's' l = Right (suffixes l)
-menu x _ = Left ("(" ++ show x ++ ")" ++ "is not supported, use (p)refix or (s)uffix")
+menu x _ = Left ("(" ++ [x] ++ ")" ++ "is not supported, use (p)refix or (s)uffix")
 
 
 -- Exercise 2
-data Expr change these variables = 
-  Number Integer
-  | Addition Expr Expr
-  | Times Expr Expr
+
+
+data Expr a =
+  Number a
+  | Plus (Expr a) (Expr a)
+  | Times (Expr a) (Expr a)
   deriving Show
 
 -- replace dummy expressions by real expressions in comments below as soon as datatype Expr has been defined
-expr1 = undefined
-expr2 = undefined
-expr3 = undefined
+expr1 = Times (Plus (Number (5.2 :: Double)) (Number 4)) (Number 2)
+expr2 = Plus (Number (2 :: Int)) (Times (Number 3) (Number 4))
+expr3 = Times (Number "hello") (Number "world")
 
--- expr1 = Times (Plus (Number (5.2 :: Double)) (Number 4)) (Number 2)
--- expr2 = Plus (Number (2 :: Int)) (Times (Number 3) (Number 4))
--- expr3 = Times (Number "hello") (Number "world")
+numbers :: Expr a -> [a]
+numbers (Number x) = [x]
+numbers (Plus x y) = numbers x ++ numbers y
+numbers (Times x y) = numbers x ++ numbers y
 
-numbers :: a        -- change the type
-numbers = undefined -- and the definition
 
-eval :: a        -- change the type
-eval = undefined -- and the definition
+eval :: Num a => Expr a -> a        -- change the type
+eval (Number x) = x
+eval (Plus x y) = eval x + eval y
+eval (Times x y) = eval x * eval y
 
-exprToString :: a        -- change the type
-exprToString = undefined -- and the definition
+exprToString :: Show a => Expr a -> String
+exprToString (Number x) = show x
+exprToString (Plus x y) = "(" ++ exprToString x ++ " + " ++ exprToString y ++ ")"
+exprToString (Times x y) = exprToString x ++ " * " ++exprToString y
+
 
 
 -- Tests: Un-comment the desired test (and :reload) after you provided a corresponding solution.
@@ -53,7 +59,7 @@ testEval1 = "Expected 18.4; eval expr1 returned " ++ show (eval expr1 :: Double)
 testEval2 = "Expected 14; eval expr2 returned " ++ show (eval expr2 :: Int)
 testEval = putStr (testEval1 ++ "\n" ++ testEval2 ++ "\n")
 
-testExprToString1 = "Expected \"(5.2 + 4.0) * 2.0\"; exprToString expr1 returned " ++ exprToString expr1
-testExprToString2 = "Expected \"2 + 3 * 4\"; exprToString expr2 returned " ++ exprToString expr2
-testExprToString3 = "Expected \"\\\"hello\\\" * \\\"world\\\"\"; exprToString expr3 returned " ++ exprToString expr3
+testExprToString1 = "Expected \"(5.2 + 4.0) * 2.0\"; exprToString expr1 returned " ++ show (exprToString expr1 :: String)
+testExprToString2 = "Expected \"2 + 3 * 4\"; exprToString expr2 returned " ++ show (exprToString expr2 :: String)
+testExprToString3 = "Expected \"\\\"hello\\\" * \\\"world\\\"\"; exprToString expr3 returned " ++ show (exprToString expr3 :: String)
 testExprToString = putStr (testExprToString1 ++ "\n" ++ testExprToString2 ++ "\n" ++ testExprToString3 ++ "\n")
