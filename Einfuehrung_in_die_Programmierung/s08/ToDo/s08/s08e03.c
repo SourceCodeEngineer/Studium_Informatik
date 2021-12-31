9ebebd1d6aab57abc4ae09182d1f4539d107c162
SourceCodeEngineer <santino.pircher.programmierer@gmail.com>
2021-12-31 10:59:15 +0100

    added more homework

--- Einfuehrung_in_die_Programmierung/s08/ToDo/s08/s08e03.c
+++ Einfuehrung_in_die_Programmierung/s08/ToDo/s08/s08e03.c
@@ -14,26 +14,38 @@ void print_person(struct person p){
     printf("%s %s %d", p.first_name, p.last_name, p.age);
 }
 
-void age_comparator(const void *, const void *){
+void age_comparator(const void *pa, const void *pb){
+    const int *p1 = pa;
+    const int *p2 = pb;
+    return *p2 - *p1;
+}
+/*
+void first_name_comparator(const void *pa, const void *pb){
     
 }
 
-void first_name_comparator(const void *, const void *){
+void last_name_comparator(const void *pa, const void *pb){
     
 }
 
-void last_name_comparator(const void *, const void *){
+void name_comparator(person_t *pa){
     
 }
 
-void name_comparator(person_t *){
+void person_comparator(person_t *pa){
     
 }
-
-void person_comparator(person_t *){
-    
-}
-
+*/
 int main (void){
+
+    int nums[] = {0,7,6,5,8,15};
+    int n = sizeof(nums)/sizeof(int);
+
+    qsort(nums, n, sizeof(int), age_comparator);
+
+    for (int i = 0; i<n; i++){
+        printf(" %d", nums[i]);
+    }
+
     return EXIT_SUCCESS;
 }
