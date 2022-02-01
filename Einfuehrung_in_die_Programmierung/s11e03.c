a820b8ba854894da4f9be84be02d706f6e3d9a6b
SourceCodeEngineer <santino.pircher.programmierer@gmail.com>
2022-02-01 15:22:33 +0100

    added some changes

--- Einfuehrung_in_die_Programmierung/s11e03.c
+++ Einfuehrung_in_die_Programmierung/s11e03.c
@@ -4,17 +4,17 @@
 int write_n(const char *src_path, const char *dst_path, int n){
     
     int i = 0;
-    char buffer[100];
+    char buffer[256];
 
-    if(n >= 101 || n <= 0) return -1; // ERROR -1 -> n is not wetween 0 and 100!
-
-    FILE *dst = fopen(dst_path, "w");
-    if(dst == NULL) return -11; // ERROR -11 -> failed to open destination file!
+    if(n > 100 || n < 1) return -1; // ERROR -1 -> n is not wetween 0 and 100!
 
     FILE *src = fopen(src_path, "r");
     if(src == NULL) return -10; // ERROR -10 -> failed to open source file!
 
-    while (fgets(buffer, 100, src) != NULL){
+    FILE *dst = fopen(dst_path, "w");
+    if(dst == NULL) return -11; // ERROR -11 -> failed to open / create destination file!
+
+    while (fgets(buffer, 256, src) != NULL){
         i++;
         if(n == i){fprintf(dst, "%s", buffer); i = 0;}
     }
