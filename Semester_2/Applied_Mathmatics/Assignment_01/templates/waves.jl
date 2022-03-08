c3612e697e8801ecb00ac79914c6dbc67928d677
SourceCodeEngineer <s.pircher@gmx.net>
2022-03-08 14:17:46 +0100

    edited some nonesense

--- Semester_2/Applied_Mathmatics/Assignment_01/templates/waves.jl
+++ Semester_2/Applied_Mathmatics/Assignment_01/templates/waves.jl
@@ -1,6 +1,6 @@
 using Plots
 
-p = plot( 0:0.1:2π, sin, label="sine", title="Triad sound wave" )
-plot!( 0:0.1:2π,  cos, label="cosine")
+p = plot( 0:0.01:π, sin, label="sine", title="Triade sound wave")
+plot!( 0:0.01:2π,  cos, label="cosine")
 
 display(p)
