fa9ac058e7bf5a19829110b95af00457836087a3
SourceCodeEngineer <santino.pircher.programmierer@gmail.com>
2022-03-09 18:03:29 +0100

    finished julia homework 1

--- Semester_2/Applied_Mathmatics/Assignment_01/templates/waves.jl
+++ Semester_2/Applied_Mathmatics/Assignment_01/templates/waves.jl
@@ -1,13 +1,18 @@
 using Plots
 
-x = todo
-y = todo
-z = todo 
-w = todo
+amplitude = 1
 
+frequenz_E = 329.63
+frequenz_G_SHARP = 415.30
+frequenz_B = 493.88
 
-p = plot( 0:0.005:0.01, sin, label="E = 329.63 Hz", title="Triade sound wave")
-plot!( 0:0.005:0.001,  cos, label="G# = 415.30 Hz")
-plot!( 0:0.005:0.001,  cos, label="B = 493.88 Hz")
-plot!( 0:0.005:0.001,  cos, label="triad")
+k(t) = amplitude  * sin(2 * pi * frequenz_E * t)
+l(t) = amplitude  * sin(2 * pi * frequenz_G_SHARP * t)
+m(t) = amplitude  * sin(2 * pi * frequenz_B * t)
+n(t) = k(t) + l(t) + m(t) 
+
+p = plot(0:0.00001:0.01, k , label="E = 329.63 Hz", title="Triade sound wave", xlabel="t (seconds)", ylabel="y(t)")
+plot!(0:0.00001:0.01,  l , label="G# = 415.30 Hz")
+plot!(0:0.00001:0.01, m , label="B = 493.88 Hz")
+plot!(0:0.00001:0.01, n , label="triad")
 display(p)
