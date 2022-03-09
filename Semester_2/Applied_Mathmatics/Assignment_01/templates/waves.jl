0ae6c4a36aa5dff914a01cf544c6cc33368176f9
SourceCodeEngineer <s.pircher@gmx.net>
2022-03-09 10:49:44 +0100

    made some variable changes

--- Semester_2/Applied_Mathmatics/Assignment_01/templates/waves.jl
+++ Semester_2/Applied_Mathmatics/Assignment_01/templates/waves.jl
@@ -1,5 +1,11 @@
 using Plots
 
+x = todo
+y = todo
+z = todo 
+w = todo
+
+
 p = plot( 0:0.005:0.01, sin, label="E = 329.63 Hz", title="Triade sound wave")
 plot!( 0:0.005:0.001,  cos, label="G# = 415.30 Hz")
 plot!( 0:0.005:0.001,  cos, label="B = 493.88 Hz")
