b252ee3f9f2d70941a4ad60dc7f2a32c792b5f51
SourceCodeEngineer <santino.pircher.programmierer@gmail.com>
2022-03-16 18:30:26 +0100

    finished the homework

--- Semester_2/Programmiermethodik/at/ac/uibk/pm/g05/csaz7620/s01/e03/test.java
+++
@@ -1,9 +0,0 @@
-package at.ac.uibk.pm.g05.csaz7620.s01.e03;
-
-public class test {
-
-    public static void main(String[] args) {
-        // Prints "Hello World!" to the terminal window.
-        System.out.println("Hello World!");
-    }
-}
