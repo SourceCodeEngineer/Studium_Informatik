7498e3af4ce1f1f5044b27aff1b710267916cfe7
SourceCodeEngineer <s.pircher@gmx.net>
2022-03-17 08:19:38 +0100

    finished task 3

--- Semester_2/Operating_Systems/Aufgabe02/task3/even.c
+++ Semester_2/Operating_Systems/Aufgabe02/task3/even.c
@@ -1,48 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <errno.h>   // for errno
+#include <errno.h> // for errno
 #include <limits.h>
+#include <ctype.h>
+#include <string.h>
 
-bool isNumber(char number[])
+int checkForMinus(char argv[])
 {
-    int i = 0;
-
-    //checking for negative numbers
-    if (number[0] == '-')
-        i = 1;
-    for (; number[i] != 0; i++)
+    if (argv[0] == '-')
     {
-        //if (number[i] > '9' || number[i] < '0')
-        if (!isdigit(number[i]))
-            return false;
+        return 1;
     }
-    return true;
+    return 0;
 }
 
-int main(int argc, char argv[]){
-    if (argc > 2 || argc < 2){
-        return -2; //wromg input
-    }
-    else {
-        if (isNumber(argv[1])){
-            return -3; //not a number
+int checkForChar(char array[], int length){
+    for (int i = 0; i < length; ++i){
+        if((array[i] > 47 && array[i] < 58)){
+            continue;
+        }else{
+            return 1;
         }
-        else{
-            char *p;
-            int num;
+    }
+    return 0;
+}
 
-            errno = 0;
+int main(int argc, char *argv[])
+{
+    if (argc > 2 || argc < 2)
+    {
+        return 2; // wrong input
+    }
+    else
+    {
+        int returnValue = checkForMinus(argv[1]); // returns 1 if string starts with a '-'
 
-            long conv = strtol(argv[1], &p, 10);
+        if (returnValue)
+        {
+            // negative number
+            int stringLen = strlen(argv[1]);
+            char array[stringLen];
+            strcpy(array, argv[1]);
 
-            num = conv;
+            array[0] = '0';
 
-            if(num % 2 == 1){
-                return -1; // odd number
+            int checkedForChar = checkForChar(array, stringLen);
+
+            if (checkedForChar == 1){
+                return 3;
             }
-            else{
-                return 0; // even number
+
+            int numberToCompare = atoi(array);
+
+            switch (numberToCompare % 2)
+            {
+            case 1:
+                return 1;
+
+            default:
+                return 0;
+            }
+        }
+        else
+        {
+            // check if a char is in the string 
+            int stringLen = strlen(argv[1]);
+            char array[stringLen];
+            strcpy(array, argv[1]);
+
+            int checkedForChar = checkForChar(array, stringLen);
+
+            if (checkedForChar == 1){
+                return 3;
+            }
+
+            int numberToCompare = atoi(argv[1]);
+            switch (numberToCompare % 2)
+            {
+            case 1:
+                return 1;
+
+            default:
+                return 0;
             }
         }
     }
