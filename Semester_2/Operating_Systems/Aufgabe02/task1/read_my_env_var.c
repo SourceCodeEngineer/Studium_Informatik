6aa643a7b566345811d5e19a4f3dbc2a1742bf23
SourceCodeEngineer <s.pircher@gmx.net>
2022-03-17 10:00:04 +0100

    change the output of the even program and modified the makefies that they fit the standards

--- Semester_2/Operating_Systems/Aufgabe02/task1/read_my_env_var.c
+++ Semester_2/Operating_Systems/Aufgabe02/task1/read_my_env_var.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BUFFER 80
 
-int main(void){
-    return EXIT_SUCCESS;
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Usage: ENVIRONMENT_VARIABLE");
+        return EXIT_FAILURE;
+    }
+
+    // Make sure envar actually exists
+    if(!getenv(argv[1])){
+        printf("%s is not set\n", argv[1]);
+        return EXIT_FAILURE;
+    }else{
+        printf("%s is set to %s\n", argv[1], getenv(argv[1]));
+        return EXIT_SUCCESS;
+    }
 }
