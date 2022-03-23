6a0ac12b49763ef15ecbe9730529f16753f246ed
SourceCodeEngineer <s.pircher@gmx.net>
2022-03-23 19:23:01 +0100

    finished task1

--- Semester_2/Operating_Systems/Assignment03/task1/task1.c
+++ Semester_2/Operating_Systems/Assignment03/task1/task1.c
@@ -1,5 +1,8 @@
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 double mc_pi(int64_t S) {
     int64_t in_count = 0;
@@ -13,6 +16,31 @@ double mc_pi(int64_t S) {
     return 4 * in_count / (double)S;
 }
 
-int main(voud){
+int main(int argc, char *argv[]){
+    if(argc != 3){
+        printf("Usage: Number of child processes (N), Number of simulations (S)");
+    }
+
+    int children = atoi(argv[1]);
+    int simulations = atoi(argv[2]);
+
+    int status = 0;
+
+    // forking in a for loop to create N child processes
+    for (int i = 0; i < children; ++i){
+        pid_t pid = fork();
+
+        if (pid == 0) /* only execute this if child */
+        {
+            // do something with the command line arguments
+            // eg. execlp with argv[i]
+            double result = mc_pi(simulations);
+            printf("Child %d PID = %d. mc_pi(%d) = %f.", i , getpid(), simulations, result);
+            exit(0);
+        }
+        wait(&status);  /* only the parent waits */
+    }
+    printf("Done.");
+
     return EXIT_SUCCESS;
 }
