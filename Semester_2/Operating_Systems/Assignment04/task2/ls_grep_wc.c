c9af1a8f5d104d43d1b859271b7ad7b42e99ce40
sourcecodeengineer <s.pircher@gmx.net>
2022-03-30 16:01:24 +0100

    finished OS task1 + began with task2

--- Semester_2/Operating_Systems/Assignment04/task2/ls_grep_wc.c
+++ Semester_2/Operating_Systems/Assignment04/task2/ls_grep_wc.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+int main(void) {
+
+   
+   return EXIT_SUCCESS;
 }
--- Semester_2/Operating_Systems/Assignment04/task2/ls_wc.c
+++ Semester_2/Operating_Systems/Assignment04/task2/ls_wc.c
@@ -1,8 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+int main(void)
+{
+
+   int fd1[2]; // Used to store two ends of first pipe
+   int fd2[2]; // Used to store two ends of second pipe
+
+   pid_t p;
+
+   if (pipe(fd1) == -1) // check if pipe failed
+   {
+      fprintf(stderr, "Pipe Failed");
+      return 1;
+   }
+   if (pipe(fd2) == -1) // check if pipe failed
+   {
+      fprintf(stderr, "Pipe Failed");
+      return 1;
+   }
+
+   p = fork(); // Now forking to create a child
+
+   if (p < 0) // Check if fork failed
+   {
+      fprintf(stderr, "fork Failed");
+      return 1;
+   }
+   // Parent process
+   else if (p > 0)
+   {
+      char concat_str[100];
+
+      close(fd1[0]); // Close reading end of first pipe
+
+      // Wait for child to send a string
+      wait(NULL);
+
+      close(fd2[1]); // Close writing end of second pipe
+
+      // Read string from child, print it and close
+      // reading end.
+      read(fd2[0], concat_str, 100);
+      close(fd2[0]);
+   }
+   // Child process
+   else
+   {
+      close(fd1[1]); // Close writing end of first pipe
+
+      // Read a string using first pipe
+      
+      read(fd1[0], "", 100);
+      
+      // Close both reading ends
+      close(fd1[0]);
+      close(fd2[0]);
+
+      // Write concatenated string and close writing end
+      write(fd2[1], "", strlen("") + 1);
+      close(fd2[1]);
+
+      exit(0);
+   }
+
+   return EXIT_SUCCESS;
 }
