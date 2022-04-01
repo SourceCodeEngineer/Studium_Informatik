96ae9dec62a26ac7f6271b198a0fc64b7c38ff25
sourcecodeengineer <s.pircher@gmx.net>
2022-04-01 18:11:56 +0100

    pushed because wanted to work on laptop

--- Semester_2/Operating_Systems/Assignment04/task3/task3_client.c
+++ Semester_2/Operating_Systems/Assignment04/task3/task3_client.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define DO_OR_DIE(x, s) \
    do                   \
@@ -14,13 +16,36 @@
       }                 \
    } while (0)
 
-int main(int arc, char **argc)
+int main(int argc, char **argv)
 {
 
-    for (int i = 0; i < argc-1; ++i){
-        // calling the client by forking n times!
-        
-    }
+   int client_to_server;
+   char *myfifo = "/tmp/client_to_server_fifo";
+
+   int server_to_client;
+   char *myfifo2 = "/tmp/server_to_client_fifo";
+
+   char str[BUFSIZ];
+   printf("Input message to server: ");
+   scanf("%s", str);
+
+
+   /* write str to the FIFO */
+   client_to_server = open(myfifo, O_WRONLY);
+   server_to_client = open(myfifo2, O_RDONLY);
+   write(client_to_server, str, sizeof(str));
+
+   perror("Write:"); //Very crude error check
+
+   read(server_to_client,str,sizeof(str));
+
+   perror("Read:"); // Very crude error check
+
+   printf("...received from the server: %s\n",str);
+   close(client_to_server);
+   close(server_to_client);
+
+   /* remove the FIFO */
 
    return EXIT_SUCCESS;
 }
--- Semester_2/Operating_Systems/Assignment04/task3/task3_server.c
+++ Semester_2/Operating_Systems/Assignment04/task3/task3_server.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define DO_OR_DIE(x, s) \
    do                   \
@@ -14,8 +15,61 @@
       }                 \
    } while (0)
 
-int main(int arc, char **argc)
+int main(int argc, char **argv)
 {
+   if(argc < 2){
+      printf("usage: execute the server and give it a parameter of clients (e.g.: ./task3_server euler gaus)!");
+      return EXIT_FAILURE;
+   }
 
-   return EXIT_SUCCESS;
+   int client_to_server;
+   char *myfifo = "/tmp/client_to_server_fifo";
+
+   int server_to_client;
+   char *myfifo2 = "/tmp/server_to_client_fifo";
+
+   char buf[BUFSIZ];
+
+   /* create the FIFO (named pipe) */
+   mkfifo(myfifo, 0666);
+   mkfifo(myfifo2, 0666);
+
+   /* open, read, and display the message from the FIFO */
+   client_to_server = open(myfifo, O_RDONLY);
+   server_to_client = open(myfifo2, O_WRONLY);
+
+   printf("Server ON.\n");
+
+   // execute client file with parameter argv[i]
+   // write out message that client x has connected.
+
+   
+
+   while (1)
+   {
+      read(client_to_server, buf, BUFSIZ);
+
+      if (strcmp("exit",buf)==0)
+      {
+         printf("Server OFF.\n");
+         break;
+      }
+
+      else if (strcmp("",buf)!=0)
+      {
+         printf("Received: %s\n", buf);
+         printf("Sending back...\n");
+         write(server_to_client,buf,BUFSIZ);
+      }
+
+      /* clean buf from any data */
+      memset(buf, 0, sizeof(buf));
+   }
+
+   close(client_to_server);
+   close(server_to_client);
+
+   unlink(myfifo);
+   unlink(myfifo2);
+   return 0;
 }
