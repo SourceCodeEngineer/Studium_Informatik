7e2a98f5d3c106ee03b95164a974c15263e18ba1
sourcecodeengineer <s.pircher@gmx.net>
2022-04-12 22:36:39 +0100

    finished task 1

--- Semester_2/Operating_Systems/2022/Assignment05/task1/task1.c
+++ Semester_2/Operating_Systems/2022/Assignment05/task1/task1.c
@@ -11,8 +11,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <ctype.h>
-#include <limits.h>  // for INT_MAX, INT_MIN
-#include <stdlib.h>  // for strtol
+#include <limits.h> // for INT_MAX, INT_MIN
+#include <stdlib.h> // for strtol
+#include <stdint.h>
 
 #define DO_OR_DIE(x, s) \
     do                  \
@@ -26,7 +27,8 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc != 3){
+    if (argc != 3)
+    {
         printf("Usage: ./task1 producer(number) consumer(number)\n");
         return EXIT_FAILURE;
     }
@@ -36,7 +38,8 @@ int main(int argc, char *argv[])
     errno = 0;
     long producer = strtol(argv[1], &p, 10);
 
-    if (errno != 0 || *p != '\0' || producer > INT_MAX || producer < INT_MIN) {
+    if (errno != 0 || *p != '\0' || producer > INT_MAX || producer < INT_MIN)
+    {
         // error handling
         printf("producer fail\n");
         printf("Usage: ./task1 producer(number) consumer(number)\n");
@@ -45,63 +48,79 @@ int main(int argc, char *argv[])
 
     long consumer = strtol(argv[2], &p, 10);
 
-    if (errno != 0 || *p != '\0' || consumer > INT_MAX || consumer < INT_MIN) {
+    if (errno != 0 || *p != '\0' || consumer > INT_MAX || consumer < INT_MIN)
+    {
         // error handling
         printf("consumer fail\n");
         printf("Usage: ./task1 producer(number) consumer(number)\n");
         return EXIT_FAILURE;
     }
 
-    if (consumer < 1 || producer < 1) {
+    if (consumer < 1 || producer < 1)
+    {
         printf("number must be greater then 0!\n");
         return EXIT_FAILURE;
     }
 
     // todo: input is correct and we can continue with the shared memory we need of size consumer!
+    uint64_t *sharedmemory = mmap(NULL, (consumer + 1) * sizeof(uint64_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // share memory
+    uint64_t *array = sharedmemory;
 
     // after creating shared memory fork 2 times
-
     pid_t pid = fork();
     DO_OR_DIE(pid, "fork1 failed!\n");
 
-    if (pid == 0){
+    if (pid == 0)
+    {
         // child process 1
 
         // if Producer > Consumer then wrap around the ring buffer
-        for (int i = 0; i < producer; ++i){
+        for (int i = 0; i < producer; ++i)
+        {
 
-            // write to shared memory
-            if (i > consumer){
-
-                // wrap around in ring buffer (i % buffer)
-
-            }
-
-            // normal writing to shared memory at position i
+            // wrap around in ring buffer (i % buffer)
+            array[i % consumer] = i + 1;
         }
+        munmap(sharedmemory, (consumer + 1) * sizeof(uint64_t));
     }
-
-    while ((pid = wait(NULL)) != -1);
 
     pid = fork();
     DO_OR_DIE(pid, "fork2 failed!\n");
 
-    if (pid == 0){
+    if (pid == 0)
+    {
         // child process 2
-        long result = 0;
+        unsigned long long result = 0;
 
-        for (int i = 0; i < producer; ++i){
+        for (int i = 0; i < producer; ++i)
+        {
             // read from shared memory
+            result += array[i];
         }
-
-        fprintf( stdout, "%ld\n", result);
+        array[consumer + 1] = result;
+        munmap(sharedmemory, (consumer + 1) * sizeof(uint64_t)); // sets the memory free (mmap)
     }
 
     // waiting for children
-    while ((pid = wait(NULL)) != -1);
+    while ((pid = wait(NULL)) != -1)
+        ;
+
+    // printing the result
+    printf("%ld\n", array[consumer + 1]);
 
     // cleanup
-
+    munmap(sharedmemory, (consumer + 1) * sizeof(uint64_t));
 
     return EXIT_SUCCESS;
 }
+
+/*
+    Analyze the obtained output. Is the computed result the expected value?
+    Does the computed value change across different runs? What happens when you
+    try different and larger values for N and B, for example 10,000 or 100,000?
+    Try to explain the behavior.
+
+    The output can change if the input number is high enough!
+    If the
+
+*/
