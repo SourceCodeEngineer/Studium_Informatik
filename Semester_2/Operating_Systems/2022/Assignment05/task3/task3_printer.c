39220fe19f0c63bcdedf8b15218c702b5cd40d98
sourcecodeengineer <s.pircher@gmx.net>
2022-04-16 18:02:22 +0100

    finished task 3 and made optimisation

--- Semester_2/Operating_Systems/2022/Assignment05/task3/task3_printer.c
+++ Semester_2/Operating_Systems/2022/Assignment05/task3/task3_printer.c
@@ -1,58 +1,74 @@
 #include <fcntl.h>
-#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <semaphore.h>
-#include <sys/wait.h>
 #include <mqueue.h>
 #include <errno.h>
 
+// begin user defines
+#define MESSAGEQLENGTH 10
+
+#define DO_OR_DIE(x, s) \
+    do                  \
+    {                   \
+        if ((x) < 0)    \
+        {               \
+            perror(s);  \
+            exit(1);    \
+        }               \
+    } while (0)
+// end user defines
+
+// begin main
 int main(int argc, char **argv)
 {
-    if (argc < 1)
+    if (argc != 4)
     {
+        printf("Usage ./task3_printer /abba 127 \"$(< test.txt)\"\n");
         return EXIT_FAILURE;
     }
 
-    int prio = atoi(argv[2]);
+    unsigned int priority = atoi(argv[2]);
     unlink(argv[1]);
+
     struct mq_attr attr = {
-        .mq_maxmsg = 10,
-        .mq_msgsize = 256,
-        .mq_curmsgs = 0,
+        .mq_maxmsg = MESSAGEQLENGTH,
+        .mq_msgsize = BUFSIZ,
+        .mq_curmsgs = 0, // elements in q
         .mq_flags = 0,
 
     };
+
     char buffer[BUFSIZ];
 
     mqd_t fd = mq_open(argv[1], O_CREAT | O_WRONLY, 0660, &attr);
+
     if (fd == -1)
     {
+        printf("failed to mq\n");
         mq_close(fd);
         unlink(argv[1]);
         return errno;
     }
-    
-  
-        read(STDIN_FILENO, buffer, sizeof(buffer));
-        int send = mq_send(fd, buffer, strlen(buffer), prio);
-        if (send == -1)
-        {
-            mq_close(fd);
-            unlink(argv[1]);
-            return errno;
-        }
-       
-    
+
+    strcpy(buffer, argv[3]);
+
+    int send = mq_send(fd, buffer, strlen(buffer), priority);
+
+    if (send == -1)
+    {
+        printf("failed to send mq\n");
+        mq_close(fd);
+        unlink(argv[1]);
+        return errno;
+    }
 
     mq_close(fd);
     unlink(argv[1]);
 
     return EXIT_SUCCESS;
 }
+// end main
--- Semester_2/Operating_Systems/2022/Assignment05/task3/task3_printer_server.c
+++ Semester_2/Operating_Systems/2022/Assignment05/task3/task3_printer_server.c
@@ -1,87 +1,157 @@
 #include <fcntl.h>
-#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <semaphore.h>
 #include <sys/wait.h>
 #include <mqueue.h>
 #include <errno.h>
 #include <time.h>
+#include <signal.h>
 
-#define SLEEPER usleep(200000)
+// begin user defines
+#define SLEEPER usleep(2000000)
 
+#define MESSAGEQLENGTH 10
+
+#define DO_OR_DIE(x, s) \
+    do                  \
+    {                   \
+        if ((x) < 0)    \
+        {               \
+            perror(s);  \
+            exit(1);    \
+        }               \
+    } while (0)
+// end user defines
+
+// global var for SIGINT catch
+mqd_t fd;
+char buffer2[BUFSIZ];
+// end
+
+// begin global structs
+struct mq_attr attr = {
+    .mq_maxmsg = MESSAGEQLENGTH,
+    .mq_msgsize = BUFSIZ,
+    .mq_curmsgs = 0, // elements in q
+    .mq_flags = 0,
+};
+// end global structs
+
+// signal handler
+void handlerSIGINT()
+{
+    write(STDOUT_FILENO, "Signal SIGINT received, shutting down\n", 38);
+    mq_close(fd);
+    unlink(buffer2);
+    exit(0);
+}
+// end signal handler
+
+// begin main function
 int main(int argc, char **argv)
 {
-    if (argc < 1)
+    // error handling for input
+    if (argc != 2)
     {
+        printf("Usage ./task3_printer_server NAME\n");
         return EXIT_FAILURE;
     }
 
+    // unlink argv[1]
     unlink(argv[1]);
-    struct mq_attr attr = {
-        .mq_maxmsg = 10,
-        .mq_msgsize = BUFSIZ,
-        .mq_curmsgs = 0,
-        .mq_flags = 0,
 
-    };
+    // copy argv1 to global
+    strcpy(buffer2, argv[1]);
+
+    // creating buffer
     char buffer[BUFSIZ] = "\0";
-    char temp[BUFSIZ] = "0";
+    char tmp[2] = "";
 
-    mqd_t fd = mq_open(argv[1], O_CREAT | O_RDONLY, 0660, &attr);
+    // open msq
+    fd = mq_open(argv[1], O_CREAT | O_RDONLY, 0660, &attr);
 
-    struct timespec tm;
-    int prio = 0;
-
-    clock_gettime(CLOCK_REALTIME, &tm);
-    tm.tv_sec += 20; // Set for 20 seconds
+    // error handling for mq
     if (fd == -1)
     {
+        printf("failed to open mq");
         mq_close(fd);
         unlink(argv[1]);
         return errno;
     }
+
+    // timehandling of idle time
+    struct timespec tm;
+
+    // creating priority of type unsigned int because of reference
+    unsigned int priority = 0;
+
+    // timehandling of idle time
+    clock_gettime(CLOCK_REALTIME, &tm);
+    tm.tv_sec += 30; // set runtime, if idle for 30 sec -> shutdown
+
+    // creating received variable
     ssize_t recieve;
-    while (0 < (recieve = mq_timedreceive(fd, buffer, sizeof(buffer) + 1, &prio, &tm)))
+
+    // loop untill we receive nothing or timeout
+    while ((recieve = mq_timedreceive(fd, buffer, sizeof(buffer) + 1, &priority, &tm)) > 0)
     {
+        // error handling
         if (recieve == -1)
         {
+            printf("receive failed");
             mq_close(fd);
             unlink(argv[1]);
             return errno;
         }
+
         pid_t pid = fork();
+        DO_OR_DIE(pid, "fork failed!\n");
+
         if (pid == 0)
         {
+            // child process
+
+            // sigint catch
+            struct sigaction sa;
+            sa.sa_handler = &handlerSIGINT;
+            sa.sa_flags = SA_RESTART;
+            sigaction(SIGINT, &sa, NULL);
+
             if (buffer[0] != '\0')
             {
-                printf("Serving print job with priority %d:\n", prio);
-                for (int i = 0; i < strlen(buffer); i++)
-                {
-                    temp[0] = buffer[i];
-                    temp[1] = ' ';
-                    write(STDOUT_FILENO, temp, 1);
-                    SLEEPER;
-                }
+                printf("Serving print job with priorityrity %d:\n", priority);
 
+                clock_gettime(CLOCK_REALTIME, &tm);
+                tm.tv_sec += 30; // set timer again up to 30 sec, if idle -> shutdown
+
+                for (int i = 0; i < (int)strlen(buffer); ++i)
+                {
+                    tmp[0] = buffer[i];
+                    tmp[1] = '\0';
+                    write(STDOUT_FILENO, tmp, 1);
+                    SLEEPER; // for printing each char into stdout with x delay
+                }
+                write(STDOUT_FILENO, "\n", 1);
                 exit(0);
             }
         }
         else
         {
+            // parent process
+
+            // waiting for child
             waitpid(pid, NULL, 0);
-            printf("\n");
-            strcpy(buffer, "0");
+            strcpy(buffer, "\0");
         }
     }
 
     mq_close(fd);
     unlink(argv[1]);
+
     return EXIT_SUCCESS;
 }
+// end main
