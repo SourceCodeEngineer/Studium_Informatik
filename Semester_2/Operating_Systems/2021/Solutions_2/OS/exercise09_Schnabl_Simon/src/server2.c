5ef2292360232b9903d022d4116f67101f7de1df
SourceCodeEngineer <s.pircher@gmx.net>
2022-04-26 13:03:15 +0200

    changed task2

--- Semester_2/Operating_Systems/2021/Solutions_2/OS/exercise09_Schnabl_Simon/src/server2.c
+++ Semester_2/Operating_Systems/2021/Solutions_2/OS/exercise09_Schnabl_Simon/src/server2.c
@@ -136,7 +136,107 @@ int main(int argc, char **argv) {
    memset(&servaddr, 0, sizeof(struct sockaddr_in)); 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(strtol(argv[1], NULL, 10));
+    servaddr.sin_port = #include "my_array.h"
+
+#include <assert.h>
+#include <string.h>
+
+#define ARRAY_INIT_CAPACITY 4
+
+typedef struct array
+{
+	void **items;
+	size_t capacity;
+	size_t size;
+} my_array;
+
+void *my_array_init()
+{
+	my_array *array = malloc(sizeof(*array));
+	if (!array)
+	{
+		return NULL;
+	}
+	array->capacity = ARRAY_INIT_CAPACITY;
+	array->size = 0;
+	array->items = malloc(sizeof(void *) * array->capacity);
+	if (!array->items)
+	{
+		free(array);
+		return NULL;
+	}
+	return array;
+}
+
+size_t my_array_size(my_array *array)
+{
+	assert(array);
+	return array->size;
+}
+
+static int array_resize(my_array *array, size_t capacity)
+{
+	assert(array);
+	void **items = realloc(array->items, sizeof(*items) * capacity);
+	if (!items)
+	{
+		return -1;
+	}
+	array->items = items;
+	array->capacity = capacity;
+	return 0;
+}
+
+void my_array_push_back(my_array *array, void *item)
+{
+	assert(array);
+	if (array->capacity == array->size)
+		array_resize(array, array->capacity * 2);
+	array->items[array->size++] = item;
+}
+
+void *my_array_get(my_array *array, size_t idx)
+{
+	assert(array);
+	if (idx < array->size)
+		return array->items[idx];
+	return NULL;
+}
+
+void my_array_delete(my_array *array, size_t idx)
+{
+	assert(array);
+	if (idx >= array->size)
+		return;
+
+	memmove(array->items + idx, array->items + (idx + 1),
+			sizeof(*array->items) * (array->size - (idx + 1)));
+	--array->size;
+
+	if (array->size > 0 && array->size == array->capacity / 4)
+		array_resize(array, array->capacity / 2);
+}
+
+void my_array_free(my_array *array)
+{
+	assert(array);
+	free(array->items);
+	free(array);
+}
+
+void *my_array_front(my_array *array)
+{
+	return my_array_get(array, 0);
+}
+
+void *my_array_pop_front(my_array *array)
+{
+	assert(array);
+	void *data = my_array_front(array);
+	my_array_delete(array, 0);
+	return data;
+}
+htons(strtol(argv[1], NULL, 10));
   
     if ((bind(listenfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
         printf("socket bind failed...\n");
--- Semester_2/Operating_Systems/2022/Assignment05/task2/task2.c
+++ Semester_2/Operating_Systems/2022/Assignment05/task2/task2.c
@@ -79,6 +79,162 @@ int main(int argc, char *argv[])
 
     uint64_t *array = sharedmemory;
 
+    // creating sem array with every element being a sem to check if it is used or not.
+    sem_t *sema = mmap(NULL, sizeof(sema) * 2, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // 0 == read, 1 == write
+
+    if (sema == MAP_FAILED)
+    {
+        perror("mmap failed at sema array\n");
+        exit(EXIT_FAILURE);
+    }
+
+    uint64_t initVals[] = {0, ringbuffer};
+    for (int i = 0; i < 2; ++i)
+    {
+        // creating sem entry for every array element
+        if (sem_init(&sema[i], 1, initVals[i]) < 0)
+        {
+            perror("sem_init failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // after creating shared memory fork 2 times
+    pid_t pid = fork();
+    DO_OR_DIE(pid, "fork1 failed!\n");
+
+    if (pid == 0)
+    {
+        // child process 1
+        for (int i = 0; i < number; ++i)
+        {
+            sem_wait(&sema[1]);
+            sharedmemory[i] = i + 1;
+            sem_post(&sema[0]);
+        }
+        munmap(sharedmemory, (ringbuffer + 1) * sizeof(uint64_t));
+    }
+    else
+    {
+
+        pid = fork();
+        DO_OR_DIE(pid, "fork2 failed!\n");
+
+        if (pid == 0)
+        {
+            // child process 2
+            for (int i = 0; i < number; ++i)
+            {
+                sem_wait(&sema[0]);
+                sharedmemory[ringbuffer] += sharedmemory[i];
+                sem_post(&sema[1]);
+            }
+
+            munmap(sharedmemory, (ringbuffer + 1) * sizeof(uint64_t)); // sets the memory free (mmap)
+        }
+    }
+
+    // waiting for children
+    while ((pid = wait(NULL)) != -1)
+        ;
+
+    // printing the result
+    printf("Result: %ld\n", array[ringbuffer]);
+
+    // cleanup
+    munmap(sharedmemory, (ringbuffer + 1) * sizeof(uint64_t));
+
+    for (int i = 0; i < 2; ++i)
+    {
+        // destroying semaphore
+        sem_destroy(&sema[i]);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+// I did notice that task 1 is much faster then task 2 because task 2 requires to wait for the semaphores and task 1 doesn't use semaphores
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h> // for semathore obviously
+#include <string.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <limits.h> // for INT_MAX, INT_MIN
+#include <stdlib.h> // for strtol
+#include <stdint.h> // for uint64_t
+#include <errno.h>
+#include <time.h>
+
+#define MAP_ANONYMOUS 0x20
+
+#define DO_OR_DIE(x, s) \
+    do                  \
+    {                   \
+        if ((x) < 0)    \
+        {               \
+            perror(s);  \
+            exit(1);    \
+        }               \
+    } while (0)
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: ./task1 number buffer(number)\n");
+        return EXIT_FAILURE;
+    }
+
+    char *p;
+
+    errno = 0;
+    long number = strtol(argv[1], &p, 10);
+
+    if (errno != 0 || *p != '\0' || number > INT_MAX || number < INT_MIN)
+    {
+        // error handling
+        printf("number fail\n");
+        printf("Usage: ./task1 number(number) ringbuffer(number)\n");
+        return EXIT_FAILURE;
+    }
+
+    long ringbuffer = strtol(argv[2], &p, 10);
+
+    if (errno != 0 || *p != '\0' || ringbuffer > INT_MAX || ringbuffer < INT_MIN)
+    {
+        // error handling
+        printf("ringbuffer fail\n");
+        printf("Usage: ./task1 number(number) ringbuffer(number)\n");
+        return EXIT_FAILURE;
+    }
+
+    if (ringbuffer < 1 || number < 1)
+    {
+        printf("number must be greater then 0!\n");
+        return EXIT_FAILURE;
+    }
+
+    // input is correct and we can continue with the shared memory we need of size ringbuffer!
+    uint64_t *sharedmemory = mmap(NULL, (ringbuffer + 1) * sizeof(uint64_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // share memory
+
+    if (sharedmemory == MAP_FAILED)
+    {
+        perror("mmap failed at sharedmemory array\n");
+        exit(EXIT_FAILURE);
+    }
+
+    uint64_t *array = sharedmemory;
+
     // creating bool check array to see if we can read or write
     uint64_t *sharedmemoryCHECK = mmap(NULL, (ringbuffer + 1) * sizeof(uint64_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0); // share memory
 
@@ -205,5 +361,7 @@ int main(int argc, char *argv[])
 
     return EXIT_SUCCESS;
 }
+
+*/
 
 // I did notice that task 1 is much faster then task 2 because task 2 requires to wait for the semaphores and task 1 doesn't use semaphores
