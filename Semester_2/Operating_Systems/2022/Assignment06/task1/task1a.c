44dd692926cfe34f77652ec937885d491100c12a
SourceCodeEngineer <s.pircher@gmx.net>
2022-04-27 11:18:02 +0200

    finished task1a OS

---
+++ Semester_2/Operating_Systems/2022/Assignment06/task1/task1a.c
@@ -0,0 +1,59 @@
+// worked in a colaboration with --
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// start global variables
+int my_global = 1;
+// end global variables
+
+#define DO_OR_DIE(x, s) \
+    do                  \
+    {                   \
+        if ((x) < 0)    \
+        {               \
+            perror(s);  \
+            exit(1);    \
+        }               \
+    } while (0)
+
+// function for pthread_create
+void *myThreadFun()
+{
+    ++my_global;
+    return NULL;
+}
+
+int main(void)
+{
+    printf("%d\n", my_global);
+
+    pid_t pid = fork();
+    DO_OR_DIE(pid, "fork failed!");
+
+    if (pid == 0)
+    {
+        // child process
+        ++my_global;
+        exit(1);
+    }
+
+    // parent
+    // waiting for child
+    while ((pid = wait(NULL)) != -1);
+
+    printf("%d\n", my_global);
+
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, myThreadFun, NULL);
+    pthread_join(thread_id, NULL);
+
+    printf("%d\n", my_global);
+    return EXIT_SUCCESS;
+}
