5cac9c6c5b3d38ef4690bdd97a86e2dde977f54d
sourcecodeengineer <s.pircher@gmx.net>
2022-05-01 08:15:35 +0100

    finished task2 but parameters are not working yet

--- Semester_2/Operating_Systems/2022/Assignment06/task3/task3.c
+++ Semester_2/Operating_Systems/2022/Assignment06/task3/task3.c
@@ -6,7 +6,11 @@
 #include "myqueue.h"
 
 struct myqueue_head queue;
-pthread_mutex_t mutex;
+pthread_mutex_t threadMutex;
+pthread_cond_t threadCond;
+
+#define SUMUPTO 50000
+#define THREADS 500
 
 long long *consumer(void *cno)
 {
@@ -14,18 +18,19 @@ long long *consumer(void *cno)
     long long sum = 0;
     while (1)
     {
-        pthread_mutex_lock(&mutex);
-        if (!myqueue_is_empty(&queue))
+        pthread_mutex_lock(&threadMutex);
+        pthread_cond_wait(&threadCond, &threadMutex);
+
+        if (myqueue_pop(&queue) == 0)
         {
-            if (myqueue_pop(&queue) == 0)
-            {
-                pthread_mutex_unlock(&mutex);
-                break;
-            }
-            sum += 1;
+            pthread_mutex_unlock(&threadMutex);
+            break;
         }
-        pthread_mutex_unlock(&mutex);
+
+        sum += 1;
+        pthread_mutex_unlock(&threadMutex);
     }
+
     *ret = sum;
     printf("Consumer %lld sum: %lld\n", *((long long *)cno), sum);
     pthread_exit(ret);
@@ -34,36 +39,39 @@ long long *consumer(void *cno)
 int main(void)
 {
 
-    pthread_t con[500];
-    pthread_mutex_init(&mutex, NULL);
+    pthread_t con[THREADS];
+    pthread_mutex_init(&threadMutex, NULL);
+    pthread_cond_init(&threadCond, NULL);
     void *status = 0;
-    long long name[500] = {0};
+    long long name[THREADS] = {0};
 
-    for (long long i = 0; i < 500; ++i)
+    for (long long i = 0; i < THREADS; ++i)
     {
         name[i] = i;
     }
 
     myqueue_init(&queue);
 
-    for (long long i = 0; i < 500; ++i)
+    for (long long i = 0; i < THREADS; ++i)
     {
         pthread_create(&con[i], NULL, (void *)consumer, (void *)&name[i]);
     }
 
-    for (long long i = 0; i < 100000; ++i)
+    for (long long i = 0; i < SUMUPTO; ++i)
     {
         myqueue_push(&queue, 1);
+        pthread_cond_signal(&threadCond);
     }
 
-    for (long long i = 0; i < 500; ++i)
+    for (long long i = 0; i < THREADS; ++i)
     {
         myqueue_push(&queue, 0);
+        pthread_cond_signal(&threadCond);
     }
 
     long long sum_total = 0;
 
-    for (long long i = 0; i < 500; ++i)
+    for (long long i = 0; i < THREADS; ++i)
     {
         pthread_join(con[i], &status);
         sum_total += *(long long *)status;
@@ -71,6 +79,7 @@ int main(void)
 
     printf("Final sum: %lld\n", sum_total);
 
-    pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&threadMutex);
+    pthread_cond_destroy(&threadCond);
     return EXIT_SUCCESS;
 }
