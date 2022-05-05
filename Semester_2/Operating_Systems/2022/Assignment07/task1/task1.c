2ed25cba11933e964512f6695b229c4dcd50c9bb
SourceCodeEngineer <s.pircher@gmx.net>
2022-05-05 11:01:58 +0200

    created "working task 1" with some bugs

--- Semester_2/Operating_Systems/2022/Assignment07/task1/task1.c
+++ Semester_2/Operating_Systems/2022/Assignment07/task1/task1.c
@@ -1,31 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>    /* For O_* constants */
 #include <pthread.h>
+#include <errno.h>
 
-pthread_mutex_t threadMutex;
+pthread_mutex_t mutex;
 
+// user defines to change paameter
 #define THREADS 1000
+#define NUMBER_OF_ITERATIONS 10000
 
-void* threadFunction(){
+int* myThreadFunction(int x){
 
+    for (int i = 0; i < NUMBER_OF_ITERATIONS; ++i){
+        pthread_mutex_lock(&mutex);
+        ++x;
+        pthread_mutex_unlock(&mutex);
+    }
+    pthread_exit(&x);
 }
 
-int main (void){
-
+int main()
+{
     int x = 100000;
 
+    pthread_t con[THREADS];
+
+    // init the mutex
+    pthread_mutex_init(&mutex, NULL);
+    
+    // create 10000 threads
     for (int i = 0; i < THREADS; ++i){
-        // create 1000 thrads
+        pthread_create(&con[i], (int *)myThreadFunction, x, NULL);
     }
 
-
     for (int i = 0; i < THREADS; ++i){
-        // join 1000 thrads
+        pthread_join(con[i], NULL);
     }
 
-
+    printf("%d", x);
     // cleanup
-    pthread_mutex_destroy(&threadMutex);
+    pthread_mutex_destroy(&mutex);
 
     return EXIT_SUCCESS;
 }
