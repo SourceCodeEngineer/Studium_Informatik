f013ba40bd9cc63f67091c66cf2bdc2701290a9c
sourcecodeengineer <s.pircher@gmx.net>
2022-05-06 11:48:34 +0100

    created mymutex.h

--- Semester_2/Operating_Systems/2022/Assignment07/task2/usemymutex.h
+++ Semester_2/Operating_Systems/2022/Assignment07/task2/usemymutex.h
@@ -1,7 +1,9 @@
-#ifndef MYQUEUE_H_
-#define MYQUEUE_H_
-
-#include <assert.h>
-#include <stdbool.h>
-
+#ifndef USEMYMUTEX
+#if USE_MY_MUTEX
+    #define MUTEX_LOCK my_mutex_lock
+    // ... other functions
+#else
+    #define MUTEX_LOCK pthread_mutex_lock
+    // ... other functions
+#endif
 #endif
