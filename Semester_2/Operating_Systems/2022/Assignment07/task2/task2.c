fa599fe2e28fabae63bf1f54b09860610289c5b5
sourcecodeengineer <s.pircher@gmx.net>
2022-05-06 15:26:06 +0100

    finished task2, works but missing required macros

--- Semester_2/Operating_Systems/2022/Assignment07/task2/task2.c
+++ Semester_2/Operating_Systems/2022/Assignment07/task2/task2.c
@@ -7,7 +7,9 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <sys/queue.h>
+#include <stdatomic.h>
 
+// queue setup start
 struct myqueue_entry {
 	int value;
 	STAILQ_ENTRY(myqueue_entry) entries;
@@ -39,29 +41,43 @@ static int myqueue_pop(myqueue* q) {
 	free(entry);
 	return value;
 }
+// queue setup end
 
-int mutex;
+// creating atomic variable
+atomic_int atomic = 0;
 
+void my_mutex_lock(){
+    atomic = 1;
+}
+
+void my_mutex_unlock(){
+    atomic = 0;
+}
+
+// WORKS, DON'T TOUCH BELOW!!!!
 struct myqueue_head queue;
 
-long long *consumer(void *cno)
+long long *consumer()
 {
     long long *ret = malloc(sizeof(long long));
     long long sum = 0;
     
     while (1)
     {
-        use_my_mutex_lock(&mutex);
+        while(atomic != 0){
+            my_mutex_lock();
+        }
+        
         if (!myqueue_is_empty(&queue))
         {
             if (myqueue_pop(&queue) == 0)
             {
-                pthread_mutex_unlock(&mutex);
+                my_mutex_unlock();
                 break;
             }
             sum += 1;
         }
-        pthread_mutex_unlock(&mutex);
+        my_mutex_unlock();
     }
     *ret = sum;
     pthread_exit(ret);
@@ -72,31 +88,38 @@ void *queue_writer(){
         myqueue_push(&queue, 1);
     }
 
+    myqueue_push(&queue, 0);
+
     return NULL;
 }
+// WORKS, DON'T TOUCH ABOVE!!!!
+
 
 int main(void)
 {
     pthread_t con[2];
-    use_my_mutex_init(&mutex, NULL);
+
     void *status = 0;
     int name[2] = {1, 2};
 
     myqueue_init(&queue);
 
     // create thread 0 that writes to queue
-    pthread_create(&con[0], NULL, (void *)consumer, (void *)&name[0]);
+    pthread_create(&con[0], NULL, (void *)queue_writer, (void *)&name[0]);
+
+    // wait for thread 0 to push 0 million 
+    pthread_join(con[0], &status);
 
     long long sum_total = 0;
     
-    for (int i = 0; i < 5; ++i)
-    {
-        pthread_join(con[i], &status);
-        sum_total += *(long long *)status;
-    }
+    // creating thread 1 to read 10 mil 1's
+    pthread_create(&con[1], NULL, (void *)consumer, (void *)&name[1]);
+
+    // wait for thread 1
+    pthread_join(con[0], &status);
+    sum_total += *(long long *)status;
 
     printf("Final sum: %lld\n", sum_total);
 
-    pthread_mutex_destroy(&mutex);
     return EXIT_SUCCESS;
 }
