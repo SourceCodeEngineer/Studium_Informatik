3d1e5ea91b82e779348bcdc43911750e1e055c3a
sourcecodeengineer <s.pircher@gmx.net>
2022-05-06 15:48:01 +0100

    added more functionality

--- Semester_2/Operating_Systems/2022/Assignment07/task3/task3.c
+++ Semester_2/Operating_Systems/2022/Assignment07/task3/task3.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <errno.h>
+#include <time.h>
+#include <stdbool.h>
 
 #define NUMBER_OF_PLAYERS 5
 
@@ -10,11 +12,12 @@ struct Player {
     int threadNumber;
     int diceRoll;
     int rollCounter;
+    bool hasRolled;
     struct Player* next;
     struct Player* prev;
 };
 
-void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter)
+void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter, bool hasRolled)
 {
     /* 1. allocate Player */
     struct Player* new_Player
@@ -24,6 +27,7 @@ void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCoun
     new_Player->threadNumber = threadNumber;
     new_Player->diceRoll = diceRoll;
     new_Player->rollCounter = rollCounter;
+    new_Player->hasRolled = hasRolled;
  
     new_Player->next = (*head_ref);
     new_Player->prev = NULL;
@@ -36,7 +40,7 @@ void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCoun
  
 /* Given a Player as prev_Player, insert a new Player after the
  * given Player */
-void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int rollCounter)
+void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int rollCounter, bool hasRolled)
 {
     /*1. check if the given prev_Player is NULL */
     if (prev_Player == NULL) {
@@ -52,6 +56,7 @@ void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int
     new_Player->threadNumber = threadNumber;
     new_Player->diceRoll = diceRoll;
     new_Player->rollCounter = rollCounter;
+    new_Player->hasRolled = hasRolled;
  
     /* 4. Make next of new Player as next of prev_Player */
     new_Player->next = prev_Player->next;
@@ -69,7 +74,7 @@ void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int
  
 /* Given a reference (pointer to pointer) to the head
    of a DLL and an int, appends a new Player at the end  */
-void append(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter)
+void append(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter, bool hasRolled)
 {
     /* 1. allocate Player */
     struct Player* new_Player
@@ -81,6 +86,7 @@ void append(struct Player** head_ref, int threadNumber, int diceRoll, int rollCo
     new_Player->threadNumber = threadNumber;
     new_Player->diceRoll = diceRoll;
     new_Player->rollCounter = rollCounter;
+    new_Player->hasRolled = hasRolled;
  
     /* 3. This new Player is going to be the last Player, so
           make next of it as NULL*/
@@ -107,10 +113,63 @@ void append(struct Player** head_ref, int threadNumber, int diceRoll, int rollCo
     return;
 }
 
-// DO NOT TOUCH ABOVE!!!!!!!
+void deleteNode(struct Player** head_ref, struct Player* del)
+{
+    /* base case */
+    if (*head_ref == NULL || del == NULL)
+        return;
+ 
+    /* If node to be deleted is head node */
+    if (*head_ref == del)
+        *head_ref = del->next;
+ 
+    /* Change next only if node to be deleted is NOT the last node */
+    if (del->next != NULL)
+        del->next->prev = del->prev;
+ 
+    /* Change prev only if node to be deleted is NOT the first node */
+    if (del->prev != NULL)
+        del->prev->next = del->next;
+ 
+    /* Finally, free the memory occupied by del*/
+    free(del);
+    return;
+}
+// DO NOT TOUCH ABOVE EVER!!!!!!!
+
+int rollDice(){
+    srand(time(NULL));
+    return rand() % 6;
+}
+
+void* threadRoutine(){
+    // rolling dice + 1 because of range 0-5 and we want 1 - 6:
+    int roll = rollDice() + 1;
+
+    // access DLL and add the roll to the correct field
+
+    // set roundnumber in DLL +1
+
+    // determine loser (multiple possible!)
+
+    // exit if we are a loser! else wait for others and play again!
+
+    return NULL;
+}
 
 int main(void){
 
+    // creating 5 Players for the game, modifiable at the top define
+    
+    // creating starting point of DLL
+    struct Player* head = NULL;
+
+    // Adding 5 players to the DLL and setting starter parameters.
+    push(&head, 0, 0, 0);
+    push(&head, 1, 0, 0);
+    push(&head, 2, 0, 0);
+    push(&head, 3, 0, 0);
+    push(&head, 4, 0, 0);
 
 
     return EXIT_SUCCESS;
