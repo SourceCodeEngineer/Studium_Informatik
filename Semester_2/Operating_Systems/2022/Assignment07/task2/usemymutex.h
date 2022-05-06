b16c0cb09d697040e985528b153ea7ed17f1ddef
sourcecodeengineer <s.pircher@gmx.net>
2022-05-06 15:03:28 +0100

    added myqueue in task2 directly and deleted .h files

--- Semester_2/Operating_Systems/2022/Assignment07/task2/usemymutex.h
+++
@@ -1,9 +0,0 @@
-#ifndef USEMYMUTEX
-#if USE_MY_MUTEX
-    #define MUTEX_LOCK my_mutex_lock
-    // ... other functions
-#else
-    #define MUTEX_LOCK pthread_mutex_lock
-    // ... other functions
-#endif
-#endif
