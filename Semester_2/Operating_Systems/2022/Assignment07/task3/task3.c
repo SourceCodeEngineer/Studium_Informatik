2019ffe24c6edceeccdc1d3239b998c16a2afe52
sourcecodeengineer <s.pircher@gmx.net>
2022-05-07 10:11:37 +0100

    added more functionality

--- Semester_2/Operating_Systems/2022/Assignment07/task3/task3.c
+++ Semester_2/Operating_Systems/2022/Assignment07/task3/task3.c
@@ -4,10 +4,12 @@
 #include <errno.h>
 #include <time.h>
 #include <stdbool.h>
+#include <stdatomic.h>
 
 #define NUMBER_OF_PLAYERS 5
 
 pthread_mutex_t mutex;
+atomic_int atomic = 0;
 
 // creating double linked list (may be overkill but first thaought). Might change that later! DO NOT TOUCH!!!
 struct Player {
@@ -15,11 +17,12 @@ struct Player {
     int diceRoll;
     int rollCounter;
     bool hasRolled;
+    int deletedPlayer;
     struct Player* next;
     struct Player* prev;
 };
 
-void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter, bool hasRolled)
+void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter, bool hasRolled, int deletedPlayer)
 {
     /* 1. allocate Player */
     struct Player* new_Player
@@ -30,6 +33,7 @@ void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCoun
     new_Player->diceRoll = diceRoll;
     new_Player->rollCounter = rollCounter;
     new_Player->hasRolled = hasRolled;
+    new_Player->deletedPlayer = deletedPlayer;
  
     new_Player->next = (*head_ref);
     new_Player->prev = NULL;
@@ -42,7 +46,7 @@ void push(struct Player** head_ref, int threadNumber, int diceRoll, int rollCoun
  
 /* Given a Player as prev_Player, insert a new Player after the
  * given Player */
-void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int rollCounter, bool hasRolled)
+void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int rollCounter, bool hasRolled, int deletedPlayer)
 {
     /*1. check if the given prev_Player is NULL */
     if (prev_Player == NULL) {
@@ -59,6 +63,7 @@ void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int
     new_Player->diceRoll = diceRoll;
     new_Player->rollCounter = rollCounter;
     new_Player->hasRolled = hasRolled;
+    new_Player->deletedPlayer = deletedPlayer;
  
     /* 4. Make next of new Player as next of prev_Player */
     new_Player->next = prev_Player->next;
@@ -76,7 +81,7 @@ void insertAfter(struct Player* prev_Player, int threadNumber, int diceRoll, int
  
 /* Given a reference (pointer to pointer) to the head
    of a DLL and an int, appends a new Player at the end  */
-void append(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter, bool hasRolled)
+void append(struct Player** head_ref, int threadNumber, int diceRoll, int rollCounter, bool hasRolled, int deletedPalyer)
 {
     /* 1. allocate Player */
     struct Player* new_Player
@@ -89,6 +94,7 @@ void append(struct Player** head_ref, int threadNumber, int diceRoll, int rollCo
     new_Player->diceRoll = diceRoll;
     new_Player->rollCounter = rollCounter;
     new_Player->hasRolled = hasRolled;
+    new_Player->deletedPlayer = deletedPalyer;
  
     /* 3. This new Player is going to be the last Player, so
           make next of it as NULL*/
@@ -189,36 +195,104 @@ bool checkIfDiceRolled(struct Player** head_ref){
     return rv;
 }
 
-void printFunctionBeforeElimination(struct Player player){
+int findSize(struct Player** node)
+{
+   int res = 0;
+   while (node != NULL)
+   {
+       ++res;
+       *node = (*node)->next;
+   }
+   return res;
+}
+
+void searchAndUpdate(struct Player** head_ref, int x, int roll)
+{
+ 
+    // Stores head Node
+    struct Player* temp = *head_ref;
+ 
+    // Stores position of the integer
+    // in the doubly linked list
+ 
+    // go through every element and if found then update entry
+    while (temp->next != NULL){
+        if (temp->threadNumber == x){
+            // update the entry
+            (*head_ref)->diceRoll = roll;
+            (*head_ref)->hasRolled = true;
+            (*head_ref)->rollCounter += 1;
+            break;
+        }
+        temp = temp->next;
+    }
+}
+
+void printFunctionBeforeElimination(struct Player** player, int x){
     pthread_mutex_lock(&mutex);
 
-    for (int i = 0; i < NUMBER_OF_PLAYERS; ++i){
-        // do printing
+    //int size = findSize(&player);
 
+    struct Player* temp = *player;
+
+    for (int i = 0; i < 5; ++i){
+        // do printing
+        if(x != 0){break;}
+
+        printf("Player %d rolled a %d\n", temp->threadNumber, temp->diceRoll);
+        //temp = temp->next;
     }
 
     pthread_mutex_unlock(&mutex);
 }
 
-void printFunctionAfterElimination(struct Player player){
+void printFunctionAfterElimination(struct Player** player){
     pthread_mutex_lock(&mutex);
+
+    // check wich player got eliminated
 
     pthread_mutex_unlock(&mutex);
 }
 
-void* threadRoutine(void* pt){
-    // rolling dice + 1 because of range 0-5 and we want 1 - 6:
-    int roll = rollDice() + 1;
+void defaultFunction(struct Player** player, int threadnumber)
+{
+    while (true)
+    {
+        // rolling dice + 1 because of range 0-5 and we want 1 - 6:
+        int roll = rollDice() + 1;
 
-    // access DLL and add the roll to the correct field
+        // find element in DLL and edit note
+        int position = search(player, threadnumber); 
+        searchAndUpdate(player, threadnumber, roll);     
+        
+        // print DLL before player(s) got eliminated
+        printFunctionBeforeElimination(player, threadnumber);
 
-    // set roundnumber in DLL +1
+        // access DLL and add the roll to the correct field
 
-    // determine loser (multiple possible!)
+        // set roundnumber in DLL +1
 
-    // exit if we are a loser! else wait for others and play again!
+        // determine loser (multiple possible!)
 
-    // reset hasrolled counter in node to false!
+        // exit if we are a loser! else wait for others and play again!
+
+        // reset hasrolled counter in node to false!
+
+        // check if eliminated then break
+        if (1 != 0){
+            break;
+        }
+    }
+}
+
+void* threadRoutine(struct Player** player){
+    pthread_mutex_lock(&mutex);
+    int x = atomic;
+    ++atomic;
+    pthread_mutex_unlock(&mutex);
+
+    // default function
+    defaultFunction(player, x);
 
     return NULL;
 }
@@ -232,11 +306,11 @@ int main(void){
     struct Player* head = NULL;
 
     // Adding 5 players to the DLL and setting starter parameters.
-    push(&head, 0, 0, 0, false);
-    push(&head, 1, 0, 0, false);
-    push(&head, 2, 0, 0, false);
-    push(&head, 3, 0, 0, false);
-    push(&head, 4, 0, 0, false);
+    push(&head, 0, 0, 0, false, -1);
+    push(&head, 1, 0, 0, false, -1);
+    push(&head, 2, 0, 0, false, -1);
+    push(&head, 3, 0, 0, false, -1);
+    push(&head, 4, 0, 0, false, -1);
 
     pthread_t con[NUMBER_OF_PLAYERS];
     int name[NUMBER_OF_PLAYERS] = {0};
@@ -248,7 +322,7 @@ int main(void){
 
     // creating 5 threads for 5 players
     for (int i = 0; i < NUMBER_OF_PLAYERS; ++i){
-        pthread_create(&con[i], NULL, (void *)threadRoutine, (void *)&name[i]);
+        pthread_create(&con[i], NULL, (void *)threadRoutine, head);
     }
 
     // waiting for 5 threads for 5 players
