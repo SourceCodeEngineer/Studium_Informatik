445d9a65d137201d207950264782ef7a771f6a21
sourcecodeengineer <s.pircher@gmx.net>
2022-05-08 11:11:43 +0100

    reworked task2 entirely, works fine now

--- Semester_2/Operating_Systems/2022/Assignment07/task2/task2.c
+++ Semester_2/Operating_Systems/2022/Assignment07/task2/task2.c
@@ -1,8 +1,15 @@
-// worked together in a colaboration with csaz7620, csaz2055, csaz7804
-
-#include <pthread.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <poll.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <errno.h>
+
 #include <unistd.h>
 #include <assert.h>
 #include <stdbool.h>
@@ -11,119 +18,143 @@
 #include <stdbool.h>
 
 // queue setup start
-struct myqueue_entry {
-	int value;
-	STAILQ_ENTRY(myqueue_entry) entries;
+struct myqueue_entry
+{
+    int value;
+    STAILQ_ENTRY(myqueue_entry)
+    entries;
 };
 
 STAILQ_HEAD(myqueue_head, myqueue_entry);
 
 typedef struct myqueue_head myqueue;
 
-static void myqueue_init(myqueue* q) {
-	STAILQ_INIT(q);
+static void myqueue_init(myqueue *q)
+{
+    STAILQ_INIT(q);
 }
 
-static bool myqueue_is_empty(myqueue* q) {
-	return STAILQ_EMPTY(q);
+static bool myqueue_is_empty(myqueue *q)
+{
+    return STAILQ_EMPTY(q);
 }
 
-static void myqueue_push(myqueue* q, int value) {
-	struct myqueue_entry* entry = malloc(sizeof(struct myqueue_entry));
-	entry->value = value;
-	STAILQ_INSERT_TAIL(q, entry, entries);
+static void myqueue_push(myqueue *q, int value)
+{
+    struct myqueue_entry *entry = malloc(sizeof(struct myqueue_entry));
+    entry->value = value;
+    STAILQ_INSERT_TAIL(q, entry, entries);
 }
 
-static int myqueue_pop(myqueue* q) {
-	assert(!myqueue_is_empty(q));
-	struct myqueue_entry* entry = STAILQ_FIRST(q);
-	const int value = entry->value;
-	STAILQ_REMOVE_HEAD(q, entries);
-	free(entry);
-	return value;
+static int myqueue_pop(myqueue *q)
+{
+    assert(!myqueue_is_empty(q));
+    struct myqueue_entry *entry = STAILQ_FIRST(q);
+    const int value = entry->value;
+    STAILQ_REMOVE_HEAD(q, entries);
+    free(entry);
+    return value;
 }
 // queue setup end
 
-// creating atomic variable
-atomic_int atomic = 0;
+#if USE_MY_MUTEX
+volatile atomic_flag mutex = ATOMIC_FLAG_INIT;
 
-void my_mutex_lock(){
-    atomic = 1;
-}
+#define MUTEX_LOCK(mutex) my_mutex_lock();
+#define MUTEX_UNLOCK(mutex) my_mutex_unlock();
+#define MUTEX_INIT(mutex) // set clear state
 
-void my_mutex_unlock(){
-    atomic = 0;
-}
+#else
+pthread_mutex_t mutex;
+#define MUTEX_LOCK(mutex) pthread_mutex_lock(&mutex)
+#define MUTEX_UNLOCK(mutex) pthread_mutex_unlock(&mutex)
+#define MUTEX_INIT(mutex)                          \
+    if (pthread_mutex_init(&mutex, NULL) != 0)     \
+    {                                              \
+        perror("mutex init failed");           \
+        return EXIT_FAILURE;                       \
+    }
 
-// WORKS, DON'T TOUCH BELOW!!!!
-struct myqueue_head queue;
+#endif
 
-long long *consumer()
+void my_mutex_lock()
 {
-    long long *ret = malloc(sizeof(long long));
-    long long sum = 0;
-    
+    while (atomic_flag_test_and_set(&mutex))
+    {
+        // if flag isnt set than the it returns true. so the section is "locked"
+    }
+}
+void my_mutex_unlock()
+{
+    atomic_flag_clear(&mutex);
+    // clear value and "unlock"
+}
+
+myqueue queue;
+
+void *consumer(void *args)
+{
+    int count = 0;
     while (1)
     {
-        while(atomic != 0){
-            atomic_flag_test_and_set(&atomic);
-        }
-        
-        if (!myqueue_is_empty(&queue))
+        MUTEX_LOCK(mutex);
+        if (myqueue_is_empty(&queue))
         {
-            if (myqueue_pop(&queue) == 0)
-            {
-                atomic_flag_clear(&atomic);
-                break;
-            }
-            sum += 1;
+            MUTEX_UNLOCK(mutex);
+            continue;
         }
-        atomic_flag_clear(&atomic);
-    }
-    *ret = sum;
-    pthread_exit(ret);
-}
+        if (myqueue_pop(&queue) == 0)
+        {
+            printf("Total sum: %d\n", count);
+            MUTEX_UNLOCK(mutex);
+            break;
+        }
+        count++;
 
-void *queue_writer(){
-    for (int i = 0; i < 10000000; ++i){
+        MUTEX_UNLOCK(mutex);
+    }
+
+    pthread_exit(args);
+}
+void *producer(void *args)
+{
+    for (int i = 0; i < 1000 * 1000; i++)
+    {
+        MUTEX_LOCK(mutex);
         myqueue_push(&queue, 1);
+        MUTEX_UNLOCK(mutex);
     }
-
+    MUTEX_LOCK(mutex);
     myqueue_push(&queue, 0);
+    MUTEX_UNLOCK(mutex);
 
-    return NULL;
+    pthread_exit(args);
 }
-// WORKS, DON'T TOUCH ABOVE!!!!
-
 
 int main(void)
 {
-    pthread_t con[2];
-
-    void *status = 0;
-    int name[2] = {1, 2};
-
+    pthread_t threads[2];
     myqueue_init(&queue);
 
-    // create thread 0 that writes to queue
-    pthread_create(&con[0], NULL, (void *)queue_writer, (void *)&name[0]);
+    MUTEX_INIT(mutex)
+    if (pthread_create(&threads[0], NULL, producer, NULL) != 0)
+    {
+        perror("Failed to create thread");
+        return EXIT_FAILURE;
+    }
+    if (pthread_create(&threads[1], NULL, consumer, NULL) != 0)
+    {
+        perror("Failed to create thread");
+        return EXIT_FAILURE;
+    }
+    for (int i = 0; i < 2; i++)
+    {
 
-    // wait for thread 0 to push 0 million 
-    pthread_join(con[0], &status);
-
-    long long sum_total = 0;
-    
-    // creating thread 1 to read 10 mil 1's
-    pthread_create(&con[1], NULL, (void *)consumer, (void *)&name[1]);
-
-    // wait for thread 0 to push 10 million 
-    pthread_join(con[0], &status);
-
-    // wait for thread 1
-    pthread_join(con[0], &status);
-    sum_total += *(long long *)status;
-
-    printf("Final sum: %lld\n", sum_total);
-
+        if (pthread_join(threads[i], NULL) != 0)
+        {
+            perror("Failed to join thread");
+            return EXIT_FAILURE;
+        }
+    }
     return EXIT_SUCCESS;
 }
