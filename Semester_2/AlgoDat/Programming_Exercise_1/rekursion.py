52b15bf524b7edf945c4daf08be629728cc8de72
sourcecodeengineer <s.pircher@gmx.net>
2022-05-08 11:37:40 +0100

    minor changes

--- Semester_2/AlgoDat/Programming_Exercise_1/rekursion.py
+++ Semester_2/AlgoDat/Programming_Exercise_1/rekursion.py
@@ -3,14 +3,18 @@ def pow(a, b):
         return a
     return a + pow(a, b-1)
 
+
 def fib(n):
     if (n == 0):
-        return 0 
+        return 0
     elif (n == 1):
         return 1
     return fib(n-1) + fib(n-2)
 
+
 def isPalindrome(str):
-    if len(str) < 2: return True
-    if str[0] != str[-1]: return False
+    if len(str) < 2:
+        return True
+    if str[0] != str[-1]:
+        return False
     return isPalindrome(str[1:-1])
