206e3c2b962f29be5c55e9320f9f09df4d097a86
sourcecodeengineer <s.pircher@gmx.net>
2022-05-27 13:37:00 +0100

    auto formatted

--- Semester_2/Operating_Systems/2022/Assignment10/task1.c
+++ Semester_2/Operating_Systems/2022/Assignment10/task1.c
@@ -12,67 +12,77 @@
 
 #define BLOCK_SIZE 1024
 
-typedef struct block_t {
+typedef struct block_t
+{
 	struct block_t *next;
-	void* mem;
+	void *mem;
 } block_t;
 
-typedef struct {
-	void* head;
-	void* tail;
+typedef struct
+{
+	void *head;
+	void *tail;
 	size_t size;
-	block_t* next_free;
+	block_t *next_free;
 } mem_pool_t;
 
-mem_pool_t global_pool; 
+mem_pool_t global_pool;
 void *global_ptr;
 pthread_mutex_t pool_mutex;
 
-void my_allocator_init(size_t size) {
-	
+void my_allocator_init(size_t size)
+{
+
 	global_pool.head = malloc(size);
 	global_pool.tail = global_pool.head + size;
 	global_pool.size = size;
 	global_pool.next_free = (block_t *)global_pool.head;
-	
+
 	pthread_mutex_init(&pool_mutex, NULL);
-	
+
 	global_ptr = global_pool.head;
 	size_t step = sizeof(block_t) + BLOCK_SIZE;
-	
-	while((global_pool.head + size) - global_ptr >= (long int) step) {
-		block_t* block = (void *) global_ptr;
-		block -> mem = global_ptr + sizeof(block_t);
-		block -> next = (block_t*)(global_ptr + step);
+
+	while ((global_pool.head + size) - global_ptr >= (long int)step)
+	{
+		block_t *block = (void *)global_ptr;
+		block->mem = global_ptr + sizeof(block_t);
+		block->next = (block_t *)(global_ptr + step);
 		global_ptr += step;
 	}
-	((block_t *) global_ptr) -> next = NULL;
+	((block_t *)global_ptr)->next = NULL;
 }
 
-void my_allocator_destroy() {
+void my_allocator_destroy()
+{
 	free(global_pool.head);
 	pthread_mutex_destroy(&pool_mutex);
 }
 
-void my_free(void *ptr) {
+void my_free(void *ptr)
+{
 	pthread_mutex_lock(&pool_mutex);
-	block_t* block = global_pool.head;
-	block -> next = global_pool.next_free;
+	block_t *block = global_pool.head;
+	block->next = global_pool.next_free;
 	global_pool.next_free = block;
 	pthread_mutex_unlock(&pool_mutex);
 }
 
-void* my_malloc(size_t size) {
+void *my_malloc(size_t size)
+{
 	pthread_mutex_lock(&pool_mutex);
-	if(size > BLOCK_SIZE) {
+	if (size > BLOCK_SIZE)
+	{
 		return NULL;
 	}
-	else if(global_pool.next_free == NULL) {
+	else if (global_pool.next_free == NULL)
+	{
 		return NULL;
 	}
-	else if(global_pool.next_free != NULL) {
-		void* ret = global_pool.next_free -> mem;
-		global_pool.next_free = global_pool.next_free -> next;
+	else if (global_pool.next_free != NULL)
+	{
+		void *ret = global_pool.next_free->mem;
+		global_pool.next_free = global_pool.next_free->next;
 		pthread_mutex_unlock(&pool_mutex);
 		return ret;
 	}
@@ -80,6 +90,7 @@ void* my_malloc(size_t size) {
 	return NULL;
 }
 
-int main() {
+int main()
+{
 	run_membench_global(my_allocator_init, my_allocator_destroy, my_malloc, my_free);
 }
--- Semester_2/Operating_Systems/2022/Assignment10/task2.c
+++ Semester_2/Operating_Systems/2022/Assignment10/task2.c
@@ -12,25 +12,28 @@
 
 #define BLOCK_SIZE 1024
 
-typedef struct block_t {
+typedef struct block_t
+{
 	struct block_t *next;
-	void* mem;
+	void *mem;
 } block_t;
 
-typedef struct {
-	void* head;
-	void* tail;
+typedef struct
+{
+	void *head;
+	void *tail;
 	size_t size;
 	size_t rem;
-	block_t* next_free;
+	block_t *next_free;
 } mem_pool_t;
 
-mem_pool_t global_pool; 
+mem_pool_t global_pool;
 void *global_ptr;
 pthread_mutex_t pool_mutex;
 
-void my_allocator_init(size_t size) {
-	
+void my_allocator_init(size_t size)
+{
+
 	global_pool.head = malloc(size);
 	global_pool.tail = global_pool.head + size;
 	global_pool.size = size;
@@ -38,43 +41,50 @@ void my_allocator_init(size_t size) {
 	global_pool.next_free = (block_t *)global_pool.head;
 	pthread_mutex_init(&pool_mutex, NULL);
 	global_ptr = global_pool.head;
-	((block_t *) global_ptr) -> next = NULL;
+	((block_t *)global_ptr)->next = NULL;
 }
 
-void my_allocator_destroy() {
+void my_allocator_destroy()
+{
 	free(global_pool.head);
 	pthread_mutex_destroy(&pool_mutex);
 }
 
-void my_free(void *ptr) {
+void my_free(void *ptr)
+{
 	pthread_mutex_lock(&pool_mutex);
-	block_t* block = global_pool.head;
-	block -> next = global_pool.next_free;
+	block_t *block = global_pool.head;
+	block->next = global_pool.next_free;
 	global_pool.next_free = block;
 	pthread_mutex_unlock(&pool_mutex);
 }
 
-void* my_malloc(size_t size) {
+void *my_malloc(size_t size)
+{
 	pthread_mutex_lock(&pool_mutex);
-	if(size > global_pool.size) {
+	if (size > global_pool.size)
+	{
 		return NULL;
 	}
-	else if(size > global_pool.rem) {
+	else if (size > global_pool.rem)
+	{
 		return NULL;
 	}
-	else if(global_pool.next_free != NULL) {
+	else if (global_pool.next_free != NULL)
+	{
 		global_pool.rem -= size;
-		global_pool.next_free -> mem = (void *) size;
-		void* ret = global_pool.next_free -> mem;
-		global_pool.next_free -> next = global_pool.head;
-		global_pool.next_free = global_pool.next_free -> next;
+		global_pool.next_free->mem = (void *)size;
+		void *ret = global_pool.next_free->mem;
+		global_pool.next_free->next = global_pool.head;
+		global_pool.next_free = global_pool.next_free->next;
 		pthread_mutex_unlock(&pool_mutex);
 		return ret;
 	}
-	pthread_mutex_unlock(&pool_mutex); 
+	pthread_mutex_unlock(&pool_mutex);
 	return NULL;
 }
 
-int main() {
+int main()
+{
 	run_membench_global(my_allocator_init, my_allocator_destroy, my_malloc, my_free);
 }
