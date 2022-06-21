50be9bb3efaaba9bf0935b2d2b7c87321de46a10
sourcecodeengineer <s.pircher@gmx.net>
2022-06-21 09:53:35 +0100

    made final changes

--- Semester_2/Operating_Systems/2022/Assignment12/myqueue.h
+++ Semester_2/Operating_Systems/2022/Assignment12/myqueue.h
@@ -6,8 +6,15 @@
 #include <stdlib.h>
 #include <sys/queue.h>
 
-struct myqueue_entry {
-	int value;
+
+struct order
+{
+	int guestID;
+	int foodID;
+};
+struct myqueue_entry
+{
+	struct order order;
 	STAILQ_ENTRY(myqueue_entry) entries;
 };
 
@@ -15,27 +22,31 @@ STAILQ_HEAD(myqueue_head, myqueue_entry);
 
 typedef struct myqueue_head myqueue;
 
-static void myqueue_init(myqueue* q) {
+static void myqueue_init(myqueue * q)
+{
 	STAILQ_INIT(q);
 }
 
-static bool myqueue_is_empty(myqueue* q) {
+static bool myqueue_is_empty(myqueue * q)
+{
 	return STAILQ_EMPTY(q);
 }
 
-static void myqueue_push(myqueue* q, int value) {
-	struct myqueue_entry* entry = malloc(sizeof(struct myqueue_entry));
-	entry->value = value;
+static void myqueue_push(myqueue * q, struct order order)
+{
+	struct myqueue_entry * entry = (struct myqueue_entry *)malloc(sizeof(struct myqueue_entry));
+	entry->order = order;
 	STAILQ_INSERT_TAIL(q, entry, entries);
 }
 
-static int myqueue_pop(myqueue* q) {
+static struct order myqueue_pop(myqueue * q)
+{
 	assert(!myqueue_is_empty(q));
-	struct myqueue_entry* entry = STAILQ_FIRST(q);
-	const int value = entry->value;
+	struct myqueue_entry * entry = STAILQ_FIRST(q);
+	const struct order order = entry->order;
 	STAILQ_REMOVE_HEAD(q, entries);
 	free(entry);
-	return value;
+	return order;
 }
 
 #endif
--- Semester_2/Operating_Systems/2022/Assignment12/restaurant.c
+++ Semester_2/Operating_Systems/2022/Assignment12/restaurant.c
@@ -1,160 +1,283 @@
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <stdint.h>
+#define _DEFAULT_SOURCE
 
 #include "myqueue.h"
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
-int cookID = 0;
-int guestID = 0;
-int guests_total = 0;
-int orders = 0;
-uint64_t totalms = 0;
 
-pthread_mutex_t mutex;
-struct myqueue_head order_queue;
-struct myqueue_head counter;
 
-void *cook()
+typedef struct RestaurantInfo
 {
-    pthread_mutex_lock(&mutex);
-    int id = cookID;
-    ++cookID;
-    pthread_mutex_unlock(&mutex);
+	pthread_mutex_t * approachCounter;
+	pthread_mutex_t * queueMutex;
+	sem_t * queueSemaphore;
+	int * foodOnCounter;
+	myqueue * orderQueue;
+	atomic_int * currentNumberOfGuests;
+	int isNotificationAppEnabled;
+	pthread_cond_t * guestConditionVariables;
+} RestaurantInfo;
 
-    int val;
-    unsigned int seed = (unsigned)pthread_self();
-    int rand_time = (rand_r(&seed) % 500 + 100);
-    while (guests_total > 0)
-    {
-        pthread_mutex_lock(&mutex);
-        if (!myqueue_is_empty(&order_queue))
-        {
-            val = myqueue_pop(&order_queue);
-            pthread_mutex_unlock(&mutex);
-            printf("Cook %d is preparing order %d\n", id, val);
-            usleep(rand_time * 1000);
-            myqueue_push(&counter, val);
-            printf("Cook %d has placed order %d on counter\n", id, val);
-        }
-        else
-        {
-            pthread_mutex_unlock(&mutex);
-        }
-    }
+typedef struct CookInfo
+{
+	int cookID;
+	struct RestaurantInfo * restaurantInfo;
+} CookInfo;
 
-    pthread_exit(0);
+typedef struct GuestInfo
+{
+	int guestID;
+	const int totalNumberOfGuests;
+	struct RestaurantInfo * restaurantInfo;
+} GuestInfo;
+
+
+static void * ThreadCook(void * arg)
+{
+	srand((unsigned)time(NULL));
+
+	CookInfo * const cookInfo = arg;
+	RestaurantInfo * const restaurantInfo = cookInfo->restaurantInfo;
+	const int myID = cookInfo->cookID;
+	int * const foodOnCounter = restaurantInfo->foodOnCounter;
+	pthread_mutex_t * const approachCounter = restaurantInfo->approachCounter;
+	pthread_mutex_t * const queueMutex = restaurantInfo->queueMutex;
+	sem_t * const queueSemaphore = restaurantInfo->queueSemaphore;
+	myqueue * const orderQueue = restaurantInfo->orderQueue;
+	atomic_int * const currentNumberOfGuests = restaurantInfo->currentNumberOfGuests;
+	pthread_cond_t * const guestConditionVariables = restaurantInfo->guestConditionVariables;
+	const int isNotificationAppEnabled = restaurantInfo->isNotificationAppEnabled;
+
+	while (*currentNumberOfGuests > 0)
+	{
+		struct order order = {.guestID = -1, .foodID = -1};
+		sem_wait(queueSemaphore);
+		pthread_mutex_lock(queueMutex);
+		// kind of busy waiting, not ideal, might fix it later
+		if (!myqueue_is_empty(orderQueue))
+		{
+			order = myqueue_pop(orderQueue);
+		}
+		pthread_mutex_unlock(queueMutex);
+
+		if (order.foodID != -1)
+		{
+			printf("Cook %d is preparing order %d\n", myID, order.foodID);
+			const unsigned timeToWait = (unsigned)rand() % 400 + 100;
+			usleep(timeToWait * 1000);
+			printf("Cook %d has finished order %d\n", myID, order.foodID);
+			while (1)
+			{
+				pthread_mutex_lock(approachCounter);
+				if (*foodOnCounter != -1)
+				{
+					pthread_mutex_unlock(approachCounter);
+					continue;
+				}
+				*foodOnCounter = order.foodID;
+				printf("Cook %d has placed order %d on counter\n", myID, order.foodID);
+				if (isNotificationAppEnabled)
+				{
+					pthread_cond_signal(&guestConditionVariables[order.guestID]);
+				}
+				pthread_mutex_unlock(approachCounter);
+				break;
+			}
+		}
+	}
+
+	printf("Cook %d is leaving the restaurant.\n", myID);
+	return NULL;
 }
 
-void *guest()
-{
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-    pthread_mutex_lock(&mutex);
-    int id = guestID;
-    ++guestID;
-    int order = orders;
-    int val;
-    printf("Guest %d has made meal order %d\n", id, orders);
-    ++orders;
-    myqueue_push(&order_queue, id);
-    pthread_mutex_unlock(&mutex);
 
-    while (1)
-    {
-        usleep(100 * 1000);
-        pthread_mutex_lock(&mutex);
-        if (!myqueue_is_empty(&counter))
-        {
-            usleep(100 * 1000);
-            val = myqueue_pop(&counter);
-            if (val == order)
-            {
-                uint64_t ms = ts.tv_sec * 1e3 + ts.tv_nsec / 1e6;
-                printf("Guest %d has picked up order %d after %ld ms\n", id, order, ms);
-                --guests_total;
-                totalms = totalms + ms;
-                pthread_mutex_unlock(&mutex);
-                pthread_exit(0);
-            }
-            else if (!myqueue_is_empty(&counter))
-            {
-                int tmp = myqueue_pop(&counter);
-                if (tmp == order)
-                {
-                    myqueue_push(&counter, val);
-                    uint64_t ms = ts.tv_sec * 1e3 + ts.tv_nsec / 1e6;
-                    printf("Guest %d has picked up order %d after %ld ms\n", id, order, ms);
-                    --guests_total;
-                    totalms = totalms + ms;
-                    pthread_mutex_unlock(&mutex);
-                    pthread_exit(0);
-                }
-                myqueue_push(&counter, tmp);
-            }
-            myqueue_push(&counter, val);
-            pthread_mutex_unlock(&mutex);
-        }
-        else
-        {
-            pthread_mutex_unlock(&mutex);
-        }
-    }
-    pthread_exit(0);
+static void * ThreadGuest(void * arg)
+{
+	srand((unsigned)time(NULL));
+
+	GuestInfo * const guestInfo = arg;
+	RestaurantInfo * const restaurantInfo = guestInfo->restaurantInfo;
+	const int myID = guestInfo->guestID;
+	int * const foodOnCounter = restaurantInfo->foodOnCounter;
+	pthread_mutex_t * const approachCounter = restaurantInfo->approachCounter;
+	pthread_mutex_t * const queueMutex = restaurantInfo->queueMutex;
+	sem_t * const queueSemaphore = restaurantInfo->queueSemaphore;
+	myqueue * const orderQueue = restaurantInfo->orderQueue;
+	atomic_int * const currentNumberOfGuests = restaurantInfo->currentNumberOfGuests;
+	const int totalNumberOfGuests = guestInfo->totalNumberOfGuests;
+	pthread_cond_t * const guestConditionVariables = restaurantInfo->guestConditionVariables;
+	const int isNotificationAppEnabled = restaurantInfo->isNotificationAppEnabled;
+
+	const int myFoodID = rand() % 10;
+	const struct order order = {.guestID = myID, .foodID = myFoodID};
+	pthread_mutex_lock(queueMutex);
+	myqueue_push(orderQueue, order);
+	pthread_mutex_unlock(queueMutex);
+	sem_post(queueSemaphore);
+	printf("Guest %d has made meal order %d\n", myID, myFoodID);
+
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	const uint64_t orderTime = (uint64_t)(ts.tv_sec * 1000 + ts.tv_nsec / 1000000);
+
+	while (1)
+	{
+		pthread_mutex_lock(approachCounter);
+		if (isNotificationAppEnabled)
+		{
+			while (*foodOnCounter != myFoodID)
+			{
+				pthread_cond_wait(&guestConditionVariables[myID], approachCounter);
+			}
+		}
+		usleep(100 * 1000);
+		if (*foodOnCounter == myFoodID)
+		{
+			*foodOnCounter = -1;
+			pthread_mutex_unlock(approachCounter);
+			break;
+		}
+		pthread_mutex_unlock(approachCounter);
+
+		usleep(100 * 1000);
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	const uint64_t getTime = (uint64_t)(ts.tv_sec * 1000 + ts.tv_nsec / 1000000);
+	printf("Guest %d has picked up order %d after %lu ms\n", myID, myFoodID, getTime - orderTime);
+	--*currentNumberOfGuests;
+
+	if (*currentNumberOfGuests == 0)
+	{
+		for (int i = 0; i < totalNumberOfGuests; ++i)
+		{
+			sem_post(queueSemaphore);
+		}
+	}
+
+	return NULL;
 }
 
-int main(int argc, char **argv)
+
+int main(int argc, char const * argv[])
 {
+	if (argc != 4)
+	{
+		fprintf(
+		  stderr,
+		  "Invalid number of arguments. Usage: ./restaurant <enable notifications> <number of guests> <number of cooks>\n");
+		return EXIT_FAILURE;
+	}
 
-    if (argc != 4)
-    {
-        perror("Usage: <enable notifications> <number of guests> <number of cooks>");
-        return EXIT_FAILURE;
-    }
+	char * endptr;
 
-    pthread_mutex_init(&mutex, NULL);
-    myqueue_init(&order_queue);
-    myqueue_init(&counter);
+	const int isNotificationAppEnabled = (int)strtol(argv[1], &endptr, 10);
+	if (endptr != argv[1] && *endptr != '\0')
+	{
+		fprintf(stderr, "Failed to parse notification enabled parameter. Aborting...\n");
+		return EXIT_FAILURE;
+	}
+	if (isNotificationAppEnabled != 0 && isNotificationAppEnabled != 1)
+	{
+		fprintf(stderr, "Notification enabling may only be 0 or 1. Aborting...\n");
+		return EXIT_FAILURE;
+	}
 
-    int COOKS = atoi(argv[3]);
-    int GUESTS = atoi(argv[2]);
-    guests_total = GUESTS;
+	const int numberOfGuests = (int)strtol(argv[2], &endptr, 10);
+	if (endptr != argv[1] && *endptr != '\0')
+	{
+		fprintf(stderr, "Failed to parse notification enabled parameter. Aborting...\n");
+		return EXIT_FAILURE;
+	}
+	if (numberOfGuests <= 0)
+	{
+		fprintf(stderr, "Number of guests must be positive. Aborting...\n");
+		return EXIT_FAILURE;
+	}
 
-    pthread_t cooks[COOKS];
-    pthread_t guests[GUESTS];
+	const int numberOfCooks = (int)strtol(argv[3], &endptr, 10);
+	if (endptr != argv[1] && *endptr != '\0')
+	{
+		fprintf(stderr, "Failed to parse notification enabled parameter. Aborting...\n");
+		return EXIT_FAILURE;
+	}
+	if (numberOfCooks <= 0)
+	{
+		fprintf(stderr, "Number of cooks must be positive. Aborting...\n");
+		return EXIT_FAILURE;
+	}
 
-    for (int i = 0; i < COOKS; ++i)
-    {
-        pthread_create(&cooks[i], NULL, cook, NULL);
-    }
+	myqueue orderQueue;
+	myqueue_init(&orderQueue);
 
-    for (int i = 0; i < GUESTS; ++i)
-    {
-        pthread_create(&guests[i], NULL, guest, NULL);
-    }
+	pthread_mutex_t approachCounter;
+	pthread_mutex_t queueMutex;
+	sem_t queueSemaphore;
+	pthread_mutex_init(&approachCounter, NULL);
+	pthread_mutex_init(&queueMutex, NULL);
+	sem_init(&queueSemaphore, 0, 0);
 
-    // if( atoi(argv[1]) == 1 ) {
+	pthread_cond_t * const guestConditionVariables =
+	  malloc(sizeof(*guestConditionVariables) * (unsigned)numberOfGuests);
+	for (int i = 0; i < numberOfGuests; ++i)
+	{
+		pthread_cond_init(&guestConditionVariables[i], NULL);
+	}
 
-    // would have been for notification
+	int foodOnCounter = -1;
+	atomic_int currentNumberOfGuests = numberOfGuests;
 
-    //}
+	RestaurantInfo restaurantInfo = {.approachCounter = &approachCounter,
+									 .queueMutex = &queueMutex,
+									 .queueSemaphore = &queueSemaphore,
+									 .foodOnCounter = &foodOnCounter,
+									 .orderQueue = &orderQueue,
+									 .currentNumberOfGuests = &currentNumberOfGuests,
+									 .isNotificationAppEnabled = isNotificationAppEnabled,
+									 .guestConditionVariables = guestConditionVariables};
 
-    for (int i = 0; i < COOKS; ++i)
-    {
-        pthread_join(cooks[i], NULL);
-    }
+	CookInfo cookInfos[numberOfCooks];
+	pthread_t cooks[numberOfCooks];
+	for (int i = 0; i < numberOfCooks; ++i)
+	{
+		cookInfos[i].cookID = i;
+		cookInfos[i].restaurantInfo = &restaurantInfo;
+		pthread_create(&cooks[i], NULL, ThreadCook, (void *)&cookInfos[i]);
+	}
 
-    for (int i = 0; i < GUESTS; ++i)
-    {
-        pthread_join(guests[i], NULL);
-    }
+	GuestInfo guestInfos[numberOfGuests];
+	pthread_t guests[numberOfGuests];
+	for (int i = 0; i < numberOfGuests; ++i)
+	{
+		guestInfos[i].guestID = i;
+		*(int *)&guestInfos[i].totalNumberOfGuests = numberOfGuests;
+		guestInfos[i].restaurantInfo = &restaurantInfo;
+		pthread_create(&guests[i], NULL, ThreadGuest, (void *)&guestInfos[i]);
+	}
 
-    printf("All guests have been served with an average waiting time of %ld ms\n", totalms / GUESTS);
+	for (int i = 0; i < numberOfGuests; ++i)
+	{
+		pthread_join(guests[i], NULL);
+	}
+	for (int i = 0; i < numberOfCooks; ++i)
+	{
+		pthread_join(cooks[i], NULL);
+	}
 
-    pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&approachCounter);
+	pthread_mutex_destroy(&queueMutex);
+	sem_destroy(&queueSemaphore);
+	for (int i = 0; i < numberOfGuests; ++i)
+	{
+		pthread_cond_destroy(&guestConditionVariables[i]);
+	}
+	free(guestConditionVariables);
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
